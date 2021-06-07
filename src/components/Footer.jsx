bef5902bb7053ac68ff68eabcdbee9ef65c5c91a
IshiS98 <ishankasandamali98@gmail.com>
2021-06-07 12:23:33 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = (props) => {
         <a
           className="badge badge-dark"
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/IshiS98"
           aria-label="My GitHub"
         >
-          Hashir Shoaib
+          Ishanka Sandamali
         </a>{" "}
         using <i className="fab fa-react" />
         <p>
